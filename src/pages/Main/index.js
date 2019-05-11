c5ebfca05843b0f7520d8ccda71b57cf2a9b4ec1
takato <leonardo.takato@soaexpert.com.br>
2019-05-11 02:57:37 -0300

    feat: add tabs and content

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
-import { Container } from './styles';
+import Tabs from '~/components/Tabs';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  Title,
+  Description,
+  CardFooter,
+  Annotation
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 312.291,00</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 20,00 recebida do Pikachu san hoje às 21:30
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
+      <Tabs />
     </Container>
   );
 }
