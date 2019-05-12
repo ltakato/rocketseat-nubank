8912c5a88eb71dd529d1c11edc78edf8e97aa686
takato <leonardo.takato@soaexpert.com.br>
2019-05-12 11:37:12 -0300

    feat: add more content for main card and animation for menu

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,3 +1,4 @@
+import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -12,12 +13,16 @@ export const Content = styled.View`
   z-index: 5;
 `;
 
-export const Card = styled.View`
+export const Card = styled(Animated.View)`
   flex: 1;
   background: #FFF;
   border-radius: 4px;
   margin: 0 20px;
   height: 100%;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
 `;
 
 export const CardHeader = styled.View`
